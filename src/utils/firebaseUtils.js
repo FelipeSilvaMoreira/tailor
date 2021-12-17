8e1a04098a7fc15700ab57cd9137d9634d0311b5
Felipe Moreira <epilef.moreira@gmail.com>
2021-12-17 00:16:32 -0300

    First

--- src/utils/firebaseUtils.js
+++ src/utils/firebaseUtils.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const prodConfig = {
-    apiKey: "***************",
-    authDomain: "***************",
-    databaseURL: "***************",
-    projectId: "***************",
-    storageBucket: "***************",
-    messagingSenderId: "***************"
+    apiKey: "AIzaSyD5l6PKFMQrRJuXyX9GoMcLU2PLZJyb8Xs",
+    authDomain: "tailores.firebaseapp.com",
+    projectId: "tailores",
+    storageBucket: "tailores.appspot.com",
+    messagingSenderId: "918609445224",
+    appId: "1:918609445224:web:491c2095bca13d9eb396dd"
 };
 
 const devConfig = {
@@ -21,5 +21,6 @@ const devConfig = {
 const config = process.env.NODE_ENV === 'production'
     ? prodConfig
     : devConfig;
+
 export const firebaseImpl = firebase.initializeApp(config);
 export const firebaseDatabase = firebase.database();
